52522ac54210f1796182b1cb773ae7b53779edbe
Richardy Tanure <richardy.tanure@hotmail.com>
2024-05-01 00:53:48 -0300

    add skeleton to sidebar

--- app/_components/StatusList.tsx
+++ app/_components/StatusList.tsx
@@ -6,29 +6,57 @@ import { useEffect, useState } from "react";
 
 
 import {SidebarStatusProps} from "./SidebarStatus"
+import { Skeleton } from "@/components/ui/skeleton";
 
 export function StatusList() {
   const [statusList, setStatusList] = useState<SidebarStatusProps[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const TIMEOUT = 15
 
-  useEffect(()=>{
+  const fetchData = async () => {
     axios.get("/api/status")
-    .then(res => {
+      .then(res => {
         setStatusList(res.data)
       })
       .catch(err => {
         console.error(err)
       })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }
+
+  useEffect(()=>{
+    fetchData()
+    const interval = setInterval(() => {
+      fetchData()
+    }, 1000 * TIMEOUT)
+
+    return () => clearInterval(interval)
   }, [])
 
   return (
     <div className="h-fit flex flex-col gap-3 relative py-4">
       <h1 className="font-semibold text-2xl sticky">My Status:</h1>
       {
-        statusList.length === 0 
-          ? (<p className="text-center text-primary">You don't have status</p>) 
-          : statusList.map((status, index)=> (
-              <SidebarStatus status={status} key={index}/>
-            ))    
+        isLoading 
+          ? (
+            <div className="flex flex-col gap-4 ">
+              <Skeleton className="w-full h-20"/>
+              <Skeleton className="w-full h-20"/>
+              <Skeleton className="w-full h-20"/>
+              <Skeleton className="w-full h-20"/>
+              <Skeleton className="w-full h-20"/>
+              <Skeleton className="w-full h-20"/>
+            </div>
+          ) 
+          : (
+            statusList.length === 0 
+              ? (<p className="text-center text-primary">You don't have status</p>) 
+              : statusList.map((status, index)=> (
+                <SidebarStatus status={status} key={index}/>
+              ))    
+          )
       }
     </div>
   )
